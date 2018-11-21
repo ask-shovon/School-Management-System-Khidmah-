b3c978fdea5210bcffde66f0adba7d2dd55b3337
ask-shovon <askshovon@gmail.com>
2018-11-21 10:14:28 +0600

    start to first change

--- Account/admin.py
+++ Account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import StudentInfo
+from .models import SchoolInfo
 
 # Register your models here.
-admin.site.register(StudentInfo)
+admin.site.register(SchoolInfo)
--- Account/models.py
+++ Account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class StudentInfo(models.Model):
+class SchoolInfo(models.Model):
     school_name = models.CharField(max_length=150)
     phone = models.CharField(max_length=150)
     address = models.TextField(blank=True, null=True)
